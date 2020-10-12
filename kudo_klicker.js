067be4b0ef780ec15ccce12a56b76a595b3248c8
CooperZA <zach@raincitysolutions.com>
2020-10-11 20:26:35 -0700

    add js kk

---
+++ kudo_klicker.js
@@ -0,0 +1,11 @@
+/*
+
+<button class="btn btn-default btn-kudo btn-xs js-view-kudos" data-entry="[&quot;Activity&quot;,4160650391]" str-on="click" str-trackable-id="ChcIBTITCJf5+b8PGAIoATCiBjgAQABIARIECgIICQ==" str-type="kudos" title="View all kudos">
+    <span class="app-icon icon-color icon-kudo icon-sm">kudos</span>
+    <span class="count count-kudos" data-kudo-count="13">13</span>
+</button>
+
+*/
+
+document.querySelectorAll('.btn-kudo').forEach((elem) => { elem.click() });
+
